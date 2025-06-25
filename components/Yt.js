2110a13d3f1661d95e2712d1d7138ad3ec02846e
siddhant <siddhant.saxena.2004@gmail.com>
2025-06-26 03:17:57 +0530

    yt demo

--- components/Yt.js
+++ components/Yt.js
@@ -3,7 +3,7 @@ const YouTubeEmbed = () => {
 
     <iframe
       className="w-full h-full aspect-video"
-      src="https://www.youtube.com/embed/4HBaHJKkE-w?si=xcwc5_q8O6XIBEiz"
+      src="https://www.youtube.com/embed/XIXHhhV5mgM?si=DTtLbe1P8pTcgjaV"
       title="YouTube video player"
      
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
