45af98cf6775f2b175726e4b7e341076cf79d13a
siddhant <siddhant.saxena.2004@gmail.com>
2025-06-26 13:21:39 +0530

    add

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -5,6 +6,9 @@ const Footer = () => {
     return (
         <div className='flex justify-center items-center p-4 bg-blue-950 text-white'>
           <p >Copyright &copy; {curryear} | All rights reserved </p>
+          <Link href={'/privacy'}><p className='ms-2 hover:underline hover:cursor-pointer'>Privacy Policy</p></Link>
+          <Link href={'/terms'}><p className='ms-2 hover:underline hover:cursor-pointer'>Terms & Conditions</p></Link>
+          <Link href={'/contact'}><p className='ms-2 hover:underline hover:cursor-pointer'>Contact Us</p></Link>
 
         </div>
     )
