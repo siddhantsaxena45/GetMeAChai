288966b9c936993d6d79d2baf089431de28c9d2d
siddhant <siddhant.saxena.2004@gmail.com>
2025-06-26 13:24:10 +0530

    add

--- components/Footer.js
+++ components/Footer.js
@@ -4,11 +4,13 @@ import React from 'react'
 const Footer = () => {
   const curryear=new Date().getFullYear()
     return (
-        <div className='flex justify-center items-center p-4 bg-blue-950 text-white'>
+        <div className='flex justify-center items-center p-4 bg-blue-950 text-white gap-4 flex-wrap'>
           <p >Copyright &copy; {curryear} | All rights reserved </p>
+          <p>
           <Link href={'/privacy'}><p className='ms-2 hover:underline hover:cursor-pointer'>Privacy Policy</p></Link>
           <Link href={'/terms'}><p className='ms-2 hover:underline hover:cursor-pointer'>Terms & Conditions</p></Link>
           <Link href={'/contact'}><p className='ms-2 hover:underline hover:cursor-pointer'>Contact Us</p></Link>
+          </p>
 
         </div>
     )
